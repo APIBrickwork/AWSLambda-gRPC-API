edbb9a434d12bdcbe55c356b564da777f3262bd7
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2016-04-22 17:46:48 +0200

    Added init-script for Chef Knife AWS credentials

--- chef-repo/initScripts/knife_awsCredentials.sh
+++ chef-repo/initScripts/knife_awsCredentials.sh
@@ -2,16 +2,27 @@
 # This script creates the aws credentials folder and files
 # author: Tobias Freundorfer
 # date: 22.04.2016
+credentialsDir=".aws"
+credentialFilename="credentials"
+ChefConfigDir="git/LabCourse-group4-SS2016/chef-repo/.chef/"
+knifeConfigFilename="knife.rb"
 
+echo '--> Creating .aws folder in the home directory and adding the credentials file'
 # Create folder and files
 cd ~
-mkdir .aws
-cd .aws
-touch credentials
+mkdir $credentialsDir
+cd $credentialsDir
+touch $credentialFilename
 
 # Write default credentials to file
-echo '[default]' >> credentials
-echo 'aws_access_key_id = Admin-key-pair-frankfurt' >> credentials
-echo 'aws_secret_access_key = ./Admin-key-pair-frankfurt.pem' >> credentials
+echo '[default]' >> $credentialFilename
+echo 'aws_access_key_id = Admin-key-pair-frankfurt' >> $credentialFilename
+echo 'aws_secret_access_key = ./Admin-key-pair-frankfurt.pem' >> $credentialFilename
+
+echo '--> Editing the knife.rb (Chef knife configuration file) to point to the previously created credentials file'
+mkdir $ChefConfigDir
+cd $ChefConfigDir
+touch $knifeConfigFilename
+echo "knife[:aws_credential_file] = \"$HOME/$credentialsDir/$credentialFilename\"" >> $knifeConfigFilename
 
 echo 'INFO: You have to copy the Key >Admin-key-pair-frankfurt.pem< into the .aws directory'
