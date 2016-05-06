e0a519a9df3ceff02827d01b81fb74c12dc7850d
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2016-05-06 20:53:41 +0200

    Removed chef-repo from this repository. Moved it to another repo.

--- chef-repo/.chef/knife.rb
+++
@@ -1,5 +0,0 @@
-knife[:aws_credential_file] = "/home/tobi/.aws/credentials"
-knife[:ssh_key_name] = "Admin-chefIO-knife"
-knife[:region] = "eu-central-1"
-knife[:local_mode] = true
-
--- chef-repo/cookbooks/example/metadata.rb
+++
@@ -1,3 +0,0 @@
-name 'example'
-description 'An example cookbook'
-version '1.0.0'
