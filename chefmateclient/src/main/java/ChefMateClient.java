6034e1a5aaa497bebc0bab18319225f88bc9be28
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2016-05-09 09:30:44 +0200

    - Initial commit consisting of the raw project structure and correct pom.xml settings

---
+++ chefmateclient/src/main/java/ChefMateClient.java
@@ -0,0 +1,17 @@
+
+/**
+ * The ChefMateClient offering connection to the ChefMateServer and its services.
+ * @author Tobias Freundorfer
+ *
+ */
+public class ChefMateClient
+{
+	// TODO: Add .proto file into "/src/main/proto" folder & generate sources
+	
+	
+	// TODO: See WebShopClient on how to set up this class!!
+	
+	// TODO: Implement service requests!
+	
+	// TODO: GUI (JavaFX? Swing?)
+}
