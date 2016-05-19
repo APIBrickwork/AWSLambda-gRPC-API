56bda507e7f2abde8770dc1dd4bebb4df081f4cf
Tobias Freundorfer <tobiasfreundorfer@gmail.com>
2016-05-19 11:30:13 +0200

    Added docker stuff

--- chefmateclient/docker/GenericWordPressScript.sh
+++ chefmateclient/docker/GenericWordPressScript.sh
@@ -1,19 +1,8 @@
-# Create VM
-(echo "createVM" 
-	&& echo "wordpress" # instance name
-	&& echo "scriptedTag" # instance tag
-	&& echo "eu-central-1" # region
-	&& echo "ami-87564feb" # image-id
-	&& echo "ubuntu" # username
-	&& echo "t2.micro" # flavor
-	&& echo "sg-79ae5d11" # security group
-	&& echo "shutdown" # shutdown client
-) | java -jar chefmateclient-1.0-jar-with-dependencies.jar -h $1 -p $2
 
 # Init chef repo
 (echo "initChefRepo" 
 	&& echo "ubuntu" # username
-	&& echo "TODO HOST??" # host
+	&& echo $3 # host
 	&& echo "chefmateserver_key.pem" # private key name
 	&& echo "10000" # ssh timeout
 	&& echo "shutdown" # shutdown client
@@ -22,7 +11,7 @@
 # Deploy WordPress with default settings
 (echo "executeCookbook" 
 	&& echo "ubuntu" # username
-	&& echo "TODO HOST??" # host
+	&& echo $3 # host
 	&& echo "chefmateserver_key.pem" # private key name
 	&& echo "10000" # ssh timeout
 	&& echo "cb-wordpress" # cookbook name
--- chefmateclient/docker/WordPressScript.sh
+++ chefmateclient/docker/WordPressScript.sh
@@ -1,19 +1,8 @@
-# Create VM
-(echo "createVM" 
-	&& echo "wordpress" # instance name
-	&& echo "scriptedTag" # instance tag
-	&& echo "eu-central-1" # region
-	&& echo "ami-87564feb" # image-id
-	&& echo "ubuntu" # username
-	&& echo "t2.micro" # flavor
-	&& echo "sg-79ae5d11" # security group
-	&& echo "shutdown" #s shutdown client
-) | java -jar chefmateclient-1.0-jar-with-dependencies.jar -h $1 -p $2
 
 # Init chef repo
 (echo "initChefRepo" 
 	&& echo "ubuntu" # username
-	&& echo "TODO HOST??" # host
+	&& echo $3 # host
 	&& echo "chefmateserver_key.pem" # private key name
 	&& echo "10000" # timeout
 	&& echo "shutdown" #s shutdown client
@@ -22,7 +11,7 @@
 # Deploy WordPress with default settings
 (echo "deployWPApp" 
 	&& echo "ubuntu" # username
-	&& echo "TODO HOST??" # host
+	&& echo $3 # host
 	&& echo "chefmateserver_key.pem" # private key name
 	&& echo "10000" # ssh timeout
 	&& echo "" # WP server name
