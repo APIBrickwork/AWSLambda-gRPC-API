cd19c7729be75977655bbb47b801240c02044a82
someshdas <somesh_au@yahoo.com>
2016-06-27 17:12:41 +0200

    functions updated

--- lambdaserver/lambdagrpcclient.js
+++ lambdaserver/lambdagrpcclient.js
@@ -6,170 +6,211 @@ var protoDescriptor = grpc.load("./lambda.proto");
 var grpcHost = "0.0.0.0";
 var grpcPort = 8080;
 
-var servicestub = new protoDescriptor.LambdaOps(grpcHost+":"+grpcPort,
-  grpc.credentials.createInsecure());
+var servicestub = new protoDescriptor.LambdaOps(grpcHost + ":" + grpcPort,
+    grpc.credentials.createInsecure());
 
-function sendAddPersmissionRequest(){
-  var request =
-  {
-     Action: 'STRING_VALUE', /* required */
-     FunctionName: 'STRING_VALUE', /* required */
-     Principal: 'STRING_VALUE', /* required */
-     StatementId: 'STRING_VALUE', /* required */
-     EventSourceToken: 'STRING_VALUE',
-     Qualifier: 'STRING_VALUE',
-     SourceAccount: 'STRING_VALUE',
-     SourceArn: 'STRING_VALUE'
-  };
-  console.log("Using request:\n" + JSON.stringify(request));
+function sendAddPersmissionRequest() {
+    var request = {
+        Action: 'STRING_VALUE',  /* required */
+        FunctionName: 'STRING_VALUE', /* required */
+        Principal: 'STRING_VALUE', /* required */
+        StatementId: 'STRING_VALUE', /* required */
+        EventSourceToken: 'STRING_VALUE',
+        Qualifier: 'STRING_VALUE',
+        SourceAccount: 'STRING_VALUE',
+        SourceArn: 'STRING_VALUE'
+    };
+    console.log("Using request:\n" + JSON.stringify(request));
 
-  servicestub.addPersmission(request,
-    function(err, response){
-      if(err){
-        console.log("Error when calling noStream: " + err);
-      }
-      else{
-        console.log("Received response:\n" + JSON.stringify(response));
-      }
-    });
+    servicestub.addPersmission(request,
+        function(err, response) {
+            if (err) {
+                console.log("Error when calling noStream: " + err);
+            } else {
+                console.log("Received response:\n" + JSON.stringify(response));
+            }
+        });
 }
 
-function sendCreateAliasRequest(){
-  var request =
-  {
-    FunctionName: 'STRING_VALUE', /* required */
-    FunctionVersion: 'STRING_VALUE', /* required */
-    Name: 'STRING_VALUE', /* required */
-    Description: 'STRING_VALUE'
-  };
-  console.log("Using request:\n" + JSON.stringify(request));
+function sendCreateAliasRequest() {
+    var request = {
+        FunctionName: 'hello-world-python2', /* required */
+        FunctionVersion: '$LATEST',/* required */
+        Name: 'secondPythonApp',/* required */
+        Description: 'Alias for python'
+    };
+    console.log("Using request:\n" + JSON.stringify(request));
 
-  servicestub.createAlias(request,
-    function(err, response){
-      if(err){
-        console.log("Error when calling noStream: " + err);
-      }
-      else{
-        console.log("Received response:\n" + JSON.stringify(response));
-      }
-    });
+    servicestub.createAlias(request,
+        function(err, response) {
+            if (err) {
+                console.log("Error when calling noStream: " + err);
+            } else {
+                console.log("Received response:\n" + JSON.stringify(response));
+            }
+        });
 }
 
-function sendCreateEventSourceMappingRequest(){
-  var request =
-  {
-    EventSourceArn: 'STRING_VALUE', /* required */
-    FunctionName: 'STRING_VALUE', /* required */
-    StartingPosition: 'TRIM_HORIZON | LATEST', /* required */
-    BatchSize: 0,
-    Enabled: true || false
-  };
-  console.log("Using request:\n" + JSON.stringify(request));
+function sendCreateEventSourceMappingRequest() {
+    var request = {
+        EventSourceArn: 'STRING_VALUE',
+        /* required */
+        FunctionName: 'STRING_VALUE',
+        /* required */
+        StartingPosition: 'TRIM_HORIZON | LATEST',
+        /* required */
+        BatchSize: 0,
+        Enabled: true || false
+    };
+    console.log("Using request:\n" + JSON.stringify(request));
 
-  servicestub.createEventSourceMapping(request,
-    function(err, response){
-      if(err){
-        console.log("Error when calling noStream: " + err);
-      }
-      else{
-        console.log("Received response:\n" + JSON.stringify(response));
-      }
-    });
+    servicestub.createEventSourceMapping(request,
+        function(err, response) {
+            if (err) {
+                console.log("Error when calling noStream: " + err);
+            } else {
+                console.log("Received response:\n" + JSON.stringify(response));
+            }
+        });
 }
 
-function sendcreateFunctionRequest(){
-  var request =
-  {
-      Code: { /* required */
-        S3Bucket: 'STRING_VALUE',
-        S3Key: 'STRING_VALUE',
-        S3ObjectVersion: 'STRING_VALUE',
-        ZipFile: new Buffer('...') || 'STRING_VALUE'
-      },
-      FunctionName: 'STRING_VALUE', /* required */
-      Handler: 'STRING_VALUE', /* required */
-      Role: 'STRING_VALUE', /* required */
-      Runtime: 'nodejs | nodejs4.3 | java8 | python2.7', /* required */
-      Description: 'STRING_VALUE',
-      MemorySize: 0,
-      Publish: true || false,
-      Timeout: 0,
-      VpcConfig: {
-        SecurityGroupIds: [
-          'STRING_VALUE',
-          /* more items */
-        ],
-        SubnetIds: [
-          'STRING_VALUE',
-          /* more items */
-        ]
-      }
-  };
-  console.log("Using request:\n" + JSON.stringify(request));
+function sendCreateFunctionRequest() {
+    var request = {
+        Code: { /* required */
+            S3Bucket: 'lambdaapi',
+            S3Key: 'LambdaFunctionOverHttps.zip',
+            S3ObjectVersion: 'JBm1hOQpBmTRm33wsUsOHSHOonO2qlHj'
+          //  ZipFile: new Buffer('...') || 'STRING_VALUE'
+        },
+        FunctionName: 'LambdaFunctionOverHttps',/* required */
+        Handler: 'LambdaFunctionOverHttps.handler',/* required */
+        Role: 'arn:aws:iam::927707579246:role/lambda_basic_execution',/* required */
+        Runtime: 'nodejs4.3',//'nodejs | nodejs4.3 | java8 | python2.7',/* required */
+        Description: 'Nodejs testing ',
+        MemorySize: 128,
+        Publish: true
+        //Timeout: 0,
+        // VpcConfig: {
+        //     SecurityGroupIds: [
+        //         'STRING_VALUE',
+        //         /* more items */
+        //     ],
+        //     SubnetIds: [
+        //         'STRING_VALUE',
+        //         /* more items */
+        //     ]
+        // }
+    };
+    console.log("Using request:\n" + JSON.stringify(request));
 
-  servicestub.createFunction(request,
-    function(err, response){
-      if(err){
-        console.log("Error when calling noStream: " + err);
-      }
-      else{
-        console.log("Received response:\n" + JSON.stringify(response));
-      }
-    });
+    servicestub.createFunction(request,
+        function(err, response) {
+            if (err) {
+                console.log("Error when calling noStream: " + err);
+            } else {
+                console.log("Received response:\n" + JSON.stringify(response));
+            }
+        });
 }
 
-function sendDeleteAliasRequest(){
-  var request =
-  {
-    FunctionName: 'STRING_VALUE', /* required */
-    Name: 'STRING_VALUE' /* required */
-  };
-  console.log("Using request:\n" + JSON.stringify(request));
+function sendDeleteAliasRequest() {
+    var request = {
+        FunctionName: 'hello-world-nodejs1',/* required */
+        Name: 'firstNodeJsApp' /* required */
+    };
+    console.log("Using request:\n" + JSON.stringify(request));
 
-  servicestub.deleteAlias(request,
-    function(err, response){
-      if(err){
-        console.log("Error when calling noStream: " + err);
-      }
-      else{
-        console.log("Received response:\n" + JSON.stringify(response));
-      }
-    });
+    servicestub.deleteAlias(request,
+        function(err, response) {
+            if (err) {
+                console.log("Error when calling noStream: " + err);
+            } else {
+                console.log("Received response:\n" + JSON.stringify(response));
+            }
+        });
 }
 
-function sendDeleteEventSourceMappingRequest(){
-  var request =
-  {
-    UUID: 'STRING_VALUE' /* required */
-  };
-  console.log("Using request:\n" + JSON.stringify(request));
+function sendDeleteEventSourceMappingRequest() {
+    var request = {
+        UUID: 'STRING_VALUE' /* required */
+    };
+    console.log("Using request:\n" + JSON.stringify(request));
 
-  servicestub.deleteEventSourceMapping(request,
-    function(err, response){
-      if(err){
-        console.log("Error when calling noStream: " + err);
-      }
-      else{
-        console.log("Received response:\n" + JSON.stringify(response));
-      }
-    });
+    servicestub.deleteEventSourceMapping(request,
+        function(err, response) {
+            if (err) {
+                console.log("Error when calling noStream: " + err);
+            } else {
+                console.log("Received response:\n" + JSON.stringify(response));
+            }
+        });
 }
 
-function sendDeleteFunctionRequest(){
-  var request =
-  {
-    FunctionName: 'STRING_VALUE', /* required */
-    Qualifier: 'STRING_VALUE'
-  };
-  console.log("Using request:\n" + JSON.stringify(request));
+function sendDeleteFunctionRequest() {
+    var request = {
+        FunctionName: 'hello-world-nodejs', //'STRING_VALUE'/* required */
+        //  Qualifier: ''//'STRING_VALUE'
+    };
+    console.log("Using request:\n" + JSON.stringify(request));
 
-  servicestub.deleteFunction(request,
-    function(err, response){
-      if(err){
-        console.log("Error when calling noStream: " + err);
-      }
-      else{
-        console.log("Received response:\n" + JSON.stringify(response));
-      }
-    });
+    servicestub.deleteFunction(request,
+        function(err, response) {
+            if (err) {
+                console.log("Error when calling deleteFunction: " + err);
+            } else {
+                console.log("Received response:\n" + JSON.stringify(response));
+            }
+        });
 }
+function sendGetFunctionRequest() {
+    var request = {
+        FunctionName: 'hello-world-nodejs1' //'STRING_VALUE'/* required */
+        //  Qualifier: ''//'STRING_VALUE'
+    };
+    console.log("Using request:\n" + JSON.stringify(request));
+
+    servicestub.getFunction(request,
+        function(err, response) {
+            if (err) {
+                console.log("Error when calling deleteFunction: " + err);
+            } else {
+                console.log("Received response:\n" + JSON.stringify(response));
+            }
+        });
+}
+function sendListFunctionsRequest() {
+    var request = {
+        //FunctionName: 'hello-world-nodejs', //'STRING_VALUE'/* required */
+        //  Qualifier: ''//'STRING_VALUE'
+    };
+    console.log("Using request:\n" + JSON.stringify(request));
+
+    servicestub.listFunctions(request,
+        function(err, response) {
+            if (err) {
+                console.log("Error when calling deleteFunction: " + err);
+            } else {
+                console.log("Received response:\n" + JSON.stringify(response));
+            }
+        });
+}
+
+function main() {
+    async.series([
+        sendListFunctionsRequest
+    ]);
+}
+
+if (require.main === module) {
+    main();
+}
+
+exports.sendAddPersmissionRequest = sendAddPersmissionRequest;
+exports.sendCreateAliasRequest = sendCreateAliasRequest;
+exports.sendCreateEventSourceMappingRequest = sendCreateEventSourceMappingRequest;
+exports.sendCreateFunctionRequest = sendCreateFunctionRequest;
+exports.sendDeleteAliasRequest = sendDeleteAliasRequest;
+exports.sendDeleteEventSourceMappingRequest = sendDeleteEventSourceMappingRequest;
+exports.sendDeleteFunctionRequest = sendDeleteFunctionRequest;
+exports.sendGetFunctionRequest = sendGetFunctionRequest;
+exports.sendListFunctionsRequest = sendListFunctionsRequest;
--- lambdaserver/lambdagrpcserver.js
+++ lambdaserver/lambdagrpcserver.js
@@ -2,158 +2,257 @@
 var AWS = require('aws-sdk');
 var grpc = require("grpc");
 var protoDescriptor = grpc.load("./lambda.proto");
-
+//AWS.config.credentials= new AWS.SharedIniFileCredentials({profile: 'adminuser'});
+AWS.config.update({
+    region: 'eu-central-1'
+});
 var host = "0.0.0.0";
-var port = process.env.LISTEN_PORT;
+//var port = process.env.LISTEN_PORT;
+var port = 8080;
 
 var users = [];
 
 // Check if it was called as required of as main
-if(require.main === module){
-  var grpcServer = getServer();
-  grpcServer.bind(host + ":" + port, grpc.ServerCredentials.createInsecure());
-  console.log("Starting Server listening on " + host + ":" + port);
-  grpcServer.start();
+if (require.main === module) {
+    var grpcServer = getServer();
+    grpcServer.bind(host + ":" + port, grpc.ServerCredentials.createInsecure());
+    console.log("Starting Server listening on " + host + ":" + port);
+    grpcServer.start();
 }
 
 function getServer() {
-  var server = new grpc.Server();
-  server.addProtoService(protoDescriptor.LambdaOps.service, {
-    addPersmission: addPersmission,
-    createAlias: createAlias,
-    createEventSourceMapping: createEventSourceMapping,
-    createFunction: createFunction,
-    deleteAlias: deleteAlias,
-    deleteEventSourceMapping: deleteEventSourceMapping,
-    deleteFunction: deleteFunction
-  });
-  return server;
+    var server = new grpc.Server();
+    server.addProtoService(protoDescriptor.LambdaOps.service, {
+        addPersmission: addPersmission,
+        createAlias: createAlias,
+        createEventSourceMapping: createEventSourceMapping,
+        createFunction: createFunction,
+        deleteAlias: deleteAlias,
+        deleteEventSourceMapping: deleteEventSourceMapping,
+        deleteFunction: deleteFunction,
+        getFunction: getFunction,
+        listFunctions: listFunctions
+    });
+    return server;
 }
 
-function addPersmission(call, callback){
-  console.log("Received request for addPermission with values:\n"
-  + JSON.stringify(call.request));
-  var lambda = new AWS.Lambda();
-  var response = lambda.addPermission(call.request, function (err, data) {
-  if (err){ console.log(err, err.stack); // an error occurred
-           return err.stack;}
-  else     {console.log(data);           // successful response
-           return data;}
-  });
-  console.log("Sending response:\n" + JSON.stringify(response));
-  callback(null, response);
+function addPersmission(call, callback) {
+    console.log("Received request for addPermission with values:\n" +
+        JSON.stringify(call.request));
+    var param = checkEmptyField(call.request);
+    var lambda = new AWS.Lambda();
+    var response;
+    lambda.addPermission(param, function(err, data) {
+        if (err) {
+            console.log("Error Error Message\n");
+            console.log(err, err.stack); // an error occurred
+            response = err;
+        } else {
+            console.log("#Success Message#\n");
+            console.log("string = " + JSON.stringify(data));
+            console.log(data); // successful response
+            response = data;
+        }
+        console.log("Sending response:\n" + JSON.stringify(response));
+        callback(null, response);
+    });
+}
+
+function createAlias(call, callback) {
+    console.log("Received request for createAlias with values:\n" +
+        JSON.stringify(call.request));
+    var param = checkEmptyField(call.request);
+    var lambda = new AWS.Lambda();
+    var response;
+    lambda.createAlias(param, function(err, data) {
+        if (err) {
+            console.log("Error Error Message\n");
+            console.log(err, err.stack); // an error occurred
+            response = err;
+        } else {
+            console.log("#Success Message#\n");
+            console.log("string = " + JSON.stringify(data)); // successful response
+          //  console.log(data);
+            response = JSON.stringify(data);
+        }
+        console.log("Sending response:\n" + JSON.stringify(response));
+        callback(null, response);
+    });
+
 }
 
 
-function createAlias(call, callback){
-  console.log("Received request for createAlias with values:\n"
-  + JSON.stringify(call.request));
-  var lambda = new AWS.Lambda();
-  var response = lambda.createAlias(call.request, function (err, data) {
-    if (err){ console.log(err, err.stack); // an error occurred
-             return err.stack;}
-    else     {console.log(data);           // successful response
-             return data;}
-  });
-  console.log("Sending response:\n" + JSON.stringify(response));
-  callback(null, response);
+function createEventSourceMapping(call, callback) {
+    console.log("Received request for createEventSourceMapping with values:\n" +
+        JSON.stringify(call.request));
+    var param = checkEmptyField(call.request);
+    var lambda = new AWS.Lambda();
+    var response;
+    lambda.createEventSourceMapping(param, function(err, data) {
+        if (err) {
+            console.log("Error Error Message\n");
+            console.log(err, err.stack); // an error occurred
+            response = err;
+        } else {
+            console.log("#Success Message#\n");
+            console.log("string = " + JSON.stringify(data));
+            console.log(data); // successful response
+            response = data;
+        }
+        console.log("Sending response:\n" + JSON.stringify(response));
+        callback(null, response);
+    });
 }
 
-
-function createEventSourceMapping(call, callback){
-  console.log("Received request for createEventSourceMapping with values:\n"
-  + JSON.stringify(call.request));
-  var lambda = new AWS.Lambda();
-  var response = lambda.createEventSourceMapping(call.request, function (err, data) {
-    if (err){ console.log(err, err.stack); // an error occurred
-             return err.stack;}
-    else     {console.log(data);           // successful response
-             return data;}
-  });
-  console.log("Sending response:\n" + JSON.stringify(response));
-  callback(null, response);
+function createFunction(call, callback) {
+    console.log("Received request for createFunction with values:\n" +
+        JSON.stringify(call.request));
+    var param = checkEmptyField(call.request);
+    var lambda = new AWS.Lambda();
+    var response;
+    lambda.createFunction(param, function(err, data) {
+        if (err) {
+            console.log("Error Error Message\n");
+            console.log(err, err.stack); // an error occurred
+            response = err;
+        } else {
+            console.log("#Success Message#\n");
+            //console.log("string = " + JSON.stringify(data));
+            console.log(data); // successful response
+          response = JSON.stringify(data);
+        }
+        console.log("Sending response:\n" + JSON.stringify(response));
+        callback(null, response);
+    });
 }
 
-function createFunction(call, callback){
-  console.log("Received request for createFunction with values:\n"
-  + JSON.stringify(call.request));
-  var lambda = new AWS.Lambda();
-  var response = lambda.createFunction(call.request, function (err, data) {
-    if (err){ console.log(err, err.stack); // an error occurred
-             return err.stack;}
-    else     {console.log(data);           // successful response
-             return data;}
-  });
-  console.log("Sending response:\n" + JSON.stringify(response));
-  callback(null, response);
-}
-function deleteAlias(call, callback){
-  console.log("Received request for deleteAlias with values:\n"
-  + JSON.stringify(call.request));
-  var lambda = new AWS.Lambda();
-  var response = lambda.deleteAlias(call.request, function (err, data) {
-    if (err){ console.log(err, err.stack); // an error occurred
-             return err.stack;}
-    else     {console.log(data);           // successful response
-             return data;}
-  });
-  console.log("Sending response:\n" + JSON.stringify(response));
-  callback(null, response);
+function deleteAlias(call, callback) {
+    console.log("Received request for deleteAlias with values:\n" +
+        JSON.stringify(call.request));
+    var param = checkEmptyField(call.request);
+    var lambda = new AWS.Lambda();
+    var response;
+    lambda.deleteAlias(param, function(err, data) {
+        if (err) {
+            console.log("Error Error Message\n");
+            console.log(err, err.stack); // an error occurred
+            response = err;
+        } else {
+            console.log("#Success Message#\n");
+            console.log("string = " + JSON.stringify(data));
+            //console.log(data); // successful response
+            response = "Alias deleted successfully";
+        }
+        console.log("Sending response:\n" + JSON.stringify(response));
+        callback(null, response);
+    });
 }
 
-function deleteEventSourceMapping(call, callback){
-  console.log("Received request for deleteEventSourceMapping with values:\n"
-  + JSON.stringify(call.request));
-  var lambda = new AWS.Lambda();
-  var response = lambda.deleteEventSourceMapping(call.request, function (err, data) {
-    if (err){ console.log(err, err.stack); // an error occurred
-             return err.stack;}
-    else     {console.log(data);           // successful response
-             return data;}
-  });
-  console.log("Sending response:\n" + JSON.stringify(response));
-  callback(null, response);
+function deleteEventSourceMapping(call, callback) {
+    console.log("Received request for deleteEventSourceMapping with values:\n" +
+        JSON.stringify(call.request));
+    var param = checkEmptyField(call.request);
+    var lambda = new AWS.Lambda();
+    var response;
+    lambda.deleteEventSourceMapping(param, function(err, data) {
+        if (err) {
+            console.log("Error Error Message\n");
+            console.log(err, err.stack); // an error occurred
+            response = err;
+        } else {
+            console.log("#Success Message#\n");
+            console.log("string = " + JSON.stringify(data));
+            console.log(data); // successful response
+            response = data;
+        }
+        console.log("Sending response:\n" + JSON.stringify(response));
+        callback(null, response);
+    });
 }
 
-function deleteFunction(call, callback){
-  console.log("Received request for deleteFunction with values:\n"
-  + JSON.stringify(call.request));
-  var lambda = new AWS.Lambda();
-  var response = lambda.deleteFunction(call.request, function (err, data) {
-    if (err){ console.log(err, err.stack); // an error occurred
-             return err.stack;}
-    else     {console.log(data);           // successful response
-             return data;}
-  });
-  console.log("Sending response:\n" + JSON.stringify(response));
-  callback(null, response);
+function deleteFunction(call, callback) {
+    console.log("Received request for deleteFunction with values:\n" +
+        JSON.stringify(call.request));
+    var param = checkEmptyField(call.request);
+    var lambda = new AWS.Lambda();
+    var response;
+    lambda.deleteFunction(param, function(err, data) {
+        if (err) {
+            console.log("Error Error Message\n");
+            console.log(err, err.stack); // an error occurred
+            response = err;
+        } else {
+            console.log("#Success Message#\n");
+            console.log("string = " + JSON.stringify(data));
+            console.log(data); // successful response
+            response = "successfully deleted the function";
+        }
+        console.log("Sending response:\n" + JSON.stringify(response));
+        callback(null, response);
+    });
 }
-// function processCalculation(calcreq){
-//   var response;
-//   var values = calcreq.values_to_use;
-//
-//   if(calcreq.type === "ADDITION"){
-//     var result = 0;
-//     for(var i=0;i<values.length;i++){
-//       result += parseInt(values[i]);
-//     }
-//     response =
-//     {
-//       map: {"ADDITION": result}
-//     };
-//   }
-//   else if(calcreq.type === "MULTIPLICATION"){
-//     var result = 1;
-//     for(var i=0;i<values.length;i++){
-//       result *= parseInt(values[i]);
-//     }
-//     response =
-//     {
-//       map: {"MULTIPLICATION": result}
-//     };
-//   }
-//   else{
-//     console.log("Wrong type: " + call.request.type);
-//   }
-//   return response;
-// }
+function getFunction(call, callback) {
+    console.log("Received request for getFunction with values:\n" +
+        JSON.stringify(call.request));
+    var param = checkEmptyField(call.request);
+    var lambda = new AWS.Lambda();
+    var response;
+    lambda.getFunction(param, function(err, data) {
+        if (err) {
+            console.log("Error Error Message\n");
+            console.log(err, err.stack); // an error occurred
+            response = err;
+        } else {
+            console.log("#Success Message#\n");
+          //  console.log("string = " + JSON.stringify(data));
+            console.log(data); // successful response
+            response = JSON.stringify(data);
+        }
+        console.log("Sending response:\n" + JSON.stringify(response));
+        callback(null, response);
+    });
+}
+
+function listFunctions(call, callback) {
+    console.log("Received request for listFunctions with values:\n" +
+        JSON.stringify(call.request));
+    var param = checkEmptyField(call.request);
+    var lambda = new AWS.Lambda();
+    var response;
+    lambda.listFunctions(param, function(err, data) {
+        if (err) {
+            console.log("Error Error Message\n");
+            console.log(err, err.stack); // an error occurred
+            response = err;
+        } else {
+            console.log("#Success Message#\n");
+            //console.log("string = " + JSON.stringify(data));
+            console.log(data); // successful response
+            response = JSON.stringify(data);
+        }
+        console.log("Sending response:\n" + JSON.stringify(response));
+        callback(null, response);
+    });
+}
+function checkEmptyField(request) {
+    var paramname = [];
+    for (var param in request) {
+        //console.log(param);
+        //console.log(request[param]);
+        console.log('type of ' + param + ' = ' + typeof request[param]);
+        if (request[param] === ''||request[param] === 0) {
+            paramname.push(param);
+        }
+        else if (typeof request[param]=== 'object') {
+          console.log("recursion");
+          checkEmptyField(request[param]);
+        }
+
+    }
+    //console.log(paramname);
+    for (var i = 0; i < paramname.length; i++) {
+        delete request[paramname[i]];
+    }
+    //console.log(request);
+    return request;
+}
